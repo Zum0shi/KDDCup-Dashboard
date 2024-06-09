3b7107e8525e2ba83008eddb6dff1d4912f616de
Zum0shi <sberloyarden@gmail.com>
2024-06-09 14:23:58 +0200

    ML + db changes

--- queries/create_tables.sql
+++ queries/create_tables.sql
@@ -1,16 +1,16 @@
 
 -- no primary key
-CREATE TABLE object (
-    course_id VARCHAR(50) NOT NULL,
-    module_id VARCHAR(50) NOT NULL,
-    category VARCHAR(50),
-    children VARCHAR(100),
-    tstart DATETIME
-);
+-- CREATE TABLE object (
+--     course_id VARCHAR(50) NOT NULL,
+--     module_id VARCHAR(50) NOT NULL,
+--     category VARCHAR(50),
+--     children VARCHAR(100),
+--     tstart DATETIME
+-- );
 
 
 -- enrollment tables
-CREATE TABLE enrollment_train (
+CREATE TABLE enrollment(
     enrollment_id INTEGER NOT NULL,
     username VARCHAR(50),
     course_id VARCHAR(50),
@@ -18,24 +18,24 @@ CREATE TABLE enrollment_train (
 );
 
 
-CREATE TABLE enrollment_valid (
-    enrollment_id INTEGER NOT NULL,
-    username VARCHAR(50),
-    course_id VARCHAR(50),
-    PRIMARY KEY (enrollment_id)
-);
+-- CREATE TABLE enrollment_valid (
+--     enrollment_id INTEGER NOT NULL,
+--     username VARCHAR(50),
+--     course_id VARCHAR(50),
+--     PRIMARY KEY (enrollment_id)
+-- );
 
 
-CREATE TABLE enrollment_test (
-    enrollment_id INTEGER NOT NULL,
-    username VARCHAR(50),
-    course_id VARCHAR(50),
-    PRIMARY KEY (enrollment_id)
-);
+-- CREATE TABLE enrollment_test (
+--     enrollment_id INTEGER NOT NULL,
+--     username VARCHAR(50),
+--     course_id VARCHAR(50),
+--     PRIMARY KEY (enrollment_id)
+-- );
 
 
 -- log tables
-CREATE TABLE log_train (
+CREATE TABLE log(
     enrollment_id INTEGER NOT NULL,
     tstamp TIMESTAMP,
     source VARCHAR(50),
@@ -44,40 +44,40 @@ CREATE TABLE log_train (
     FOREIGN KEY (enrollment_id) REFERENCES enrollment_train(enrollment_id)
 );
 
-CREATE TABLE log_valid (
-    enrollment_id INTEGER NOT NULL,
-    tstamp TIMESTAMP,
-    source VARCHAR(50),
-    logged_event VARCHAR(50),
-    object VARCHAR(50),
-    FOREIGN KEY (enrollment_id) REFERENCES enrollment_valid(enrollment_id)
-);
+-- CREATE TABLE log_valid (
+--     enrollment_id INTEGER NOT NULL,
+--     tstamp TIMESTAMP,
+--     source VARCHAR(50),
+--     logged_event VARCHAR(50),
+--     object VARCHAR(50),
+--     FOREIGN KEY (enrollment_id) REFERENCES enrollment_valid(enrollment_id)
+-- );
 
-CREATE TABLE log_test (
-    enrollment_id INTEGER NOT NULL,
-    tstamp TIMESTAMP,
-    source VARCHAR(50),
-    logged_event VARCHAR(50),
-    object VARCHAR(50),
-    FOREIGN KEY (enrollment_id) REFERENCES enrollment_test(enrollment_id)
-);
+-- CREATE TABLE log_test (
+--     enrollment_id INTEGER NOT NULL,
+--     tstamp TIMESTAMP,
+--     source VARCHAR(50),
+--     logged_event VARCHAR(50),
+--     object VARCHAR(50),
+--     FOREIGN KEY (enrollment_id) REFERENCES enrollment_test(enrollment_id)
+-- );
 
 
 -- truth tables
-CREATE TABLE truth_train (
+CREATE TABLE truth(
     enrollment_id INTEGER NOT NULL,
     dropped_out BOOLEAN,
     FOREIGN KEY (enrollment_id) REFERENCES enrollment_train(enrollment_id)
 );
 
-CREATE TABLE truth_valid (
-    enrollment_id INTEGER NOT NULL,
-    dropped_out BOOLEAN,
-    FOREIGN KEY (enrollment_id) REFERENCES enrollment_valid(enrollment_id)
-);
+-- CREATE TABLE truth_valid (
+--     enrollment_id INTEGER NOT NULL,
+--     dropped_out BOOLEAN,
+--     FOREIGN KEY (enrollment_id) REFERENCES enrollment_valid(enrollment_id)
+-- );
 
-CREATE TABLE truth_test (
-    enrollment_id INTEGER NOT NULL,
-    dropped_out BOOLEAN,
-    FOREIGN KEY (enrollment_id) REFERENCES enrollment_test(enrollment_id)
-);
+-- CREATE TABLE truth_test (
+--     enrollment_id INTEGER NOT NULL,
+--     dropped_out BOOLEAN,
+--     FOREIGN KEY (enrollment_id) REFERENCES enrollment_test(enrollment_id)
+-- );
--- queries/load_tables.sql
+++ queries/load_tables.sql
@@ -1,56 +1,56 @@
 -- object
-LOAD DATA LOCAL INFILE 'C:\\Users\\Jorda\\KDDCup 2015\\data\\ObjectData\\object.csv'
-INTO TABLE object
-FIELDS TERMINATED BY ','
-IGNORE 1 ROWS;
+-- LOAD DATA LOCAL INFILE 'C:\\Users\\Jorda\\KDDCup 2015\\data\\ObjectData\\object.csv'
+-- INTO TABLE object
+-- FIELDS TERMINATED BY ','
+-- IGNORE 1 ROWS;
 
 
 -- enrollment
 LOAD DATA LOCAL INFILE 'C:\\Users\\Jorda\\KDDCup 2015\\data\\train\\enrollment_train.csv'
-INTO TABLE enrollment_train
+INTO TABLE enrollment
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
 LOAD DATA LOCAL INFILE 'C:\\Users\\Jorda\\KDDCup 2015\\data\\validation\\enrollment_train.csv'
-INTO TABLE enrollment_train
+INTO TABLE enrollment
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
 LOAD DATA LOCAL INFILE 'C:\\Users\\Jorda\\KDDCup 2015\\data\\test\\enrollment_train.csv'
-INTO TABLE enrollment_test
+INTO TABLE enrollment
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
 
 -- log
 LOAD DATA LOCAL INFILE 'C:\\Users\\Jorda\\KDDCup 2015\\data\\train\\log_train.csv'
-INTO TABLE log_train
+INTO TABLE log
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
 LOAD DATA LOCAL INFILE 'C:\\Users\\Jorda\\KDDCup 2015\\data\\validation\\log_train.csv'
-INTO TABLE log_train
+INTO TABLE log
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
 LOAD DATA LOCAL INFILE 'C:\\Users\\Jorda\\KDDCup 2015\\data\\test\\log_train.csv'
-INTO TABLE log_test
+INTO TABLE log
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
 
 -- truth
 LOAD DATA LOCAL INFILE 'C:\\Users\\Jorda\\KDDCup 2015\\data\\train\\truth_train.csv'
-INTO TABLE truth_train
+INTO TABLE truth
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
 LOAD DATA LOCAL INFILE 'C:\\Users\\Jorda\\KDDCup 2015\\data\\validation\\truth_train.csv'
-INTO TABLE truth_train
+INTO TABLE truth
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
 LOAD DATA LOCAL INFILE 'C:\\Users\\Jorda\\KDDCup 2015\\data\\test\\truth_train.csv'
-INTO TABLE truth_test
+INTO TABLE truth
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
